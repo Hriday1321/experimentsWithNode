d90988b068bc46cb9ba8ee76fe1da261a231796b
EC2 Default User <hridaygupta100@hotmail.com>
2021-06-12 21:28:46 +0000

    test #2

--- npmDemo/demo.js
+++ npmDemo/demo.js
@@ -1,6 +1,9 @@
 var cat = require("cat-me");
 var joke = require("knock-knock-jokes");
+var fake = require("faker");
 
 console.log(joke());
+
+console.log(fake.commerce.productName());
 
 console.log(cat("nyan"));
