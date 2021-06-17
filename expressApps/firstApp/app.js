b752ac9ed4d2c14c04b5dbd2f4024d1ac22058d5
EC2 Default User <hridaygupta100@hotmail.com>
2021-06-17 18:54:21 +0000

    using param

--- expressApps/firstApp/app.js
+++ expressApps/firstApp/app.js
@@ -14,6 +14,14 @@ app.get("/dog", function(req,res){
     res.send("Hello There Woof!");
 })
 
+app.get("/r/:subreddit", function(req,res){
+    res.send(req.params.subreddit);
+})
+
+app.get("*", function(req,res){
+    res.send("You stoopid");
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started")
 })
