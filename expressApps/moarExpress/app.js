5b49abeacb4275e9400043564f25e4c0082f5afe
EC2 Default User <hridaygupta100@hotmail.com>
2021-06-22 15:01:03 +0000

    added route

--- expressApps/moarExpress/app.js
+++ expressApps/moarExpress/app.js
@@ -5,6 +5,11 @@ app.get("/", function(req,res){
     res.render("home.ejs")
 })
 
+app.get("/favFood/:food", function(req,res){
+    var foodItem = req.params.food;
+    res.render("favorite.ejs", {food: foodItem});
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started");
 })
