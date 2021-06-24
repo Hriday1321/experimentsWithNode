e21b82584645d5fc440c1e4d3a6300b62a5f9099
EC2 Default User <hridaygupta100@hotmail.com>
2021-06-24 16:59:16 +0000

    Added hate comment route

--- expressApps/moarExpress/app.js
+++ expressApps/moarExpress/app.js
@@ -10,6 +10,15 @@ app.get("/favFood/:food", function(req,res){
     res.render("favorite.ejs", {food: foodItem});
 })
 
+app.get("/comments/hate", function(req,res){
+    var comments = [
+        {content:"hate you", author:"hater1"},
+        {content:"bad person", author:"hater23"},
+        {content:"boring person", author:"hate4life"},
+    ];
+    res.render("hateComments.ejs", {comments: comments});
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started");
 })
